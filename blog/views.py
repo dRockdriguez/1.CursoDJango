2fe3e9abdfe7f528fe4813ad218ceba7f6cb336d
drockdriguez <david.rodriguez@grupoonetec.com>
2019-04-16 15:16:36 +0200

    Primera vista y template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.template import loader, Context
+from django.http import HttpResponse
+from blog.models import BlogPost
+
+def archive(request):
+    posts = BlogPost.objects.all()
+    mi_template = loader.get_template("archive.html")
+    mi_contexto = Context({ 'posts': posts })
+
+    return HttpResponse(mi_template.render(mi_contexto))
