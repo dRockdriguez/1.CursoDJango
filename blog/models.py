d1e0ee8a3825fcbc42fa95fad5bb6a82dbe33fbb
drockdriguez <david.rodriguez@grupoonetec.com>
2019-04-16 13:31:10 +0200

    mostrar campos en pantalla admin

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
+from django.contrib import admin
+
 # Create your models here.
 class BlogPost(models.Model):
     title = models.CharField(max_length=150)
     body = models.TextField()
     time = models.DateTimeField()
+
+
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'time')
+
+admin.site.register(BlogPost, BlogPostAdmin)
