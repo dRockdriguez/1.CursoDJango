9f7efcf509bf75d59ff01b59db9841ffe1d534e5
drockdriguez <david.rodriguez@grupoonetec.com>
2019-04-16 12:51:47 +0200

    .

--- ejemplo/settings.py
+++ ejemplo/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'ejemplo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'mysql_cymysql',
+        'NAME': 'ejemplo1django',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
